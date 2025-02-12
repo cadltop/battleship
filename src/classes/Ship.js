bfd36fbb8616e33dc611d1e202e52e808f38e990
cadltop <caesarfromthecross.808@gmail.com>
2025-02-12 14:38:15 +0000

    Apply prettier on files and modify script.

--- src/classes/Ship.js
+++ src/classes/Ship.js
@@ -1,15 +1,15 @@
 export default class {
-    hits = 0;
-    #sunk = false;
-    #length = null;
-    constructor(length) {
-        this.#length = length;
-    }
-    hit() {
-        this.hits++;
-        if (this.hits === this.#length) this.#sunk = true;
-    }
-    isSunk() {
-        return this.#sunk;
-    }
+  hits = 0;
+  #sunk = false;
+  #length = null;
+  constructor(length) {
+    this.#length = length;
+  }
+  hit() {
+    this.hits++;
+    if (this.hits === this.#length) this.#sunk = true;
+  }
+  isSunk() {
+    return this.#sunk;
+  }
 }
