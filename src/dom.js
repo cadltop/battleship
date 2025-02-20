52be8ebaa89b20a62866604f15c52e6fc36f9d3c
cadltop <caesarfromthecross.808@gmail.com>
2025-02-20 13:56:57 +0000

    Generate basic elements on the UI

---
+++ src/dom.js
@@ -0,0 +1,16 @@
+export function renderBoard(player) {
+    const boardsDiv = document.querySelector('div.boards');
+    
+    const playerBoardDiv = document.createElement('div');
+    playerBoardDiv.classList.add('board', player.name);
+    
+    for (let r = 0; r < player.gameboard.board.length; r++) {
+        for (let c = 0; c < player.gameboard.board[r].length; c++) {
+            const positionDiv = document.createElement('div');
+            positionDiv.classList.add('position', `${r}-${c}`);
+            playerBoardDiv.append(positionDiv);
+        }
+    }
+    
+    boardsDiv.append(playerBoardDiv);
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import "normalize.css";
 import "./index.css";
 import Player from "./classes/Player";
+import * as dom from "./dom";
 
-const real = new Player();
-const computer = new Player();
+const players = [new Player('real'), new Player('computer')];
+
+for (const player of players) {
+    dom.renderBoard(player);
+}
+
+
