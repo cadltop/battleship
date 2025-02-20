44613105e359b5497b93ae1b3074c5e4ad80b4ad
cadltop <caesarfromthecross.808@gmail.com>
2025-02-20 14:46:06 +0000

    Add basic style to page

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import "./index.css";
 import Player from "./classes/Player";
 import * as dom from "./dom";
 
-const players = [new Player('real'), new Player('computer')];
+const players = [new Player("real"), new Player("computer")];
 
 for (const player of players) {
-    dom.renderBoard(player);
+  dom.renderBoard(player);
 }
-
-
