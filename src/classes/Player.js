e2c0840053dd702dbfc957f9c25b088a9e267b7e
cadltop <caesarfromthecross.808@gmail.com>
2025-03-05 22:48:45 +0000

    Add final elements to UI and style hem.

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -5,21 +5,21 @@ export default class {
   constructor(name) {
     this.#name = name;
   }
-  
+
   get gameboard() {
     return this.#gameboard;
   }
   get name() {
     return this.#name;
   }
-  
+
   isFleetSunk() {
     const sunked = [];
-    
+
     for (const ship in this.#gameboard.fleet) {
       if (this.#gameboard.fleet[ship].isSunk()) sunked.push(ship);
     }
-    
+
     if (sunked.length === 5) return true;
     return false;
   }
--- src/index.js
+++ src/index.js
@@ -5,23 +5,49 @@ import Player from "./classes/Player";
 const players = [new Player("real"), new Player("computer")];
 const boards = [createBoard(players[0]), createBoard(players[1])];
 
-for (const player of players) {
-  player.gameboard.placeShip("carrier", [0, 0], false);
-  player.gameboard.placeShip("battleship", [0, 2], false);
-  player.gameboard.placeShip("destroyer", [0, 4], false);
-  player.gameboard.placeShip("submarine", [0, 6], false);
-  player.gameboard.placeShip("patrolBoat", [0, 8], false);
+for (const ship in players[0].gameboard.fleet) {
+  const shipDiv = document.createElement("div");
+  shipDiv.classList.add("ship", ship.toLowerCase());
+
+  for (let i = 0; i < players[0].gameboard.fleet[ship].length; i++) {
+    const shipBlockDiv = document.createElement("div");
+    shipBlockDiv.className = "block";
+    shipDiv.append(shipBlockDiv);
+  }
+
+  const shipsDiv = document.querySelector("div.ships > div.containers");
+  shipsDiv.append(shipDiv);
 }
+
+setTimeout(
+  () =>
+    alert(
+      "Select a ship by clicking it. Choose it's orientation and place " +
+        "the ship by clicking a position in your board.",
+    ),
+  5,
+);
+// for (const player of players) {
+//   player.gameboard.placeShip("carrier", [0, 0], false);
+//   player.gameboard.placeShip("battleship", [0, 2], false);
+//   player.gameboard.placeShip("destroyer", [0, 4], false);
+//   player.gameboard.placeShip("submarine", [0, 6], false);
+//   player.gameboard.placeShip("patrolBoat", [0, 8], false);
+// }
 
 setTurn(players[0]);
 
 function createBoard(player) {
+  const playerDiv = document.createElement("div");
+  playerDiv.classList.add("player", player.name);
+  const h2 = document.createElement("h2");
+  h2.innerHTML = player.name;
   const boardDiv = document.createElement("div");
-  boardDiv.classList.add("board", player.name);
+  boardDiv.className = "board";
   boardDiv.dataset.clickable = null;
   const boardsDiv = document.querySelector("div.boards");
   boardsDiv.dataset.end = false;
-  
+
   for (let r = 0; r < 10; r++) {
     for (let c = 9; c >= 0; c--) {
       const positionDiv = document.createElement("div");
@@ -33,36 +59,37 @@ function createBoard(player) {
           boardDiv.dataset.clickable === "false" ||
           player.gameboard.board[r][c].shot ||
           boardsDiv.dataset.end === "true"
-        ) return;
-        
+        )
+          return;
+
         if (!player.gameboard.receiveAttack(r, c)) {
           positionDiv.style.backgroundColor = "grey";
           setTurn(player);
         } else {
           positionDiv.style.backgroundColor = "red";
         }
-        
+
         if (player.isFleetSunk()) {
           findWinner(player.name);
           boardsDiv.dataset.end = true;
         }
       });
-      
+
       boardDiv.append(positionDiv);
     }
   }
-  
-  boardsDiv.append(boardDiv);
+  playerDiv.append(h2, boardDiv);
+  boardsDiv.append(playerDiv);
 
   return boardDiv;
 }
 function setTurn(player) {
   for (const board of boards) {
     const boardName = board.classList[1];
-    
+
     if (boardName !== player.name) {
       board.dataset.clickable = true;
-      
+
       for (const position of board.children) {
         const color = position.style.backgroundColor;
         if (color !== "red" && color !== "grey") {
@@ -71,18 +98,19 @@ function setTurn(player) {
       }
     } else {
       board.dataset.clickable = false;
-      
+
       for (const ship in player.gameboard.fleet) {
         for (const p of player.gameboard.fleet[ship].positions) {
           const positionDiv = document.querySelector(
             `div.board.${player.name} > ` +
-            `div.position[data-row="${p[0]}"][data-column="${p[1]}"]`,
+              `div.position[data-row="${p[0]}"][data-column="${p[1]}"]`,
           );
-          
-          positionDiv.style.backgroundColor = !player.gameboard.board[p[0]][p[1]]
-          .shot
-          ? "green"
-          : "red";
+
+          positionDiv.style.backgroundColor = !player.gameboard.board[p[0]][
+            p[1]
+          ].shot
+            ? "green"
+            : "red";
         }
       }
     }
@@ -95,7 +123,7 @@ function findWinner(loser) {
       return true;
     }
   }
-  
+
   return false;
 }
 
@@ -105,16 +133,16 @@ function findWinner(loser) {
     positionDiv.addEventListener("c`lick", () => {
       attackEnemy(players[0]);
     });
-    
+
     function attackEnemy(enemy) {
       const positions = boards[0].children;
       const index = Math.round(Math.random() * 99);
       const position = positions[index];
       const row = parseInt(position.dataset.row);
       const column = parseInt(position.dataset.column);
-      
+
       if (!enemy.gameboard.board[row][column].shot) {
-        position.click()
+        position.click();
         if (enemy.gameboard.board[row][column].ship) {
           attackEnemy(enemy);
         }
